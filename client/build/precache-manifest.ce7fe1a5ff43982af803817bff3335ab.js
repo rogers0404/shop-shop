774462cabe6f59d48d01c2669452b7e5f1beead7
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2022-01-25 16:54:28 -0500

    Added again to Github the e-Commerce app

---
+++ client/build/precache-manifest.ce7fe1a5ff43982af803817bff3335ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec71cbe1c4e6d2bc0ca414f20fc30be0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34a758a94e0e2908f682",
+    "url": "/static/css/main.6816d842.chunk.css"
+  },
+  {
+    "revision": "e7c962524f49bcc5454a",
+    "url": "/static/js/2.ad6855a8.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.ad6855a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a758a94e0e2908f682",
+    "url": "/static/js/main.57522afc.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://rogers0404:Alejandro.1210@cluster0.y0flo.mongodb.net/shop-shop?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
